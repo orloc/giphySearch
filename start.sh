f4e4dc9b588a96c7416d71268a633b72bd9dcf34
Grant Tepper <grant.tepper@gmail.com>
2017-07-12 05:43:11 -0700

    updating with start.js

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 #  This script should start up your service on port 8080
 
+appName=dist/app.js
 
-if [ ! -f dist/app.js ]; then
+if [ ! -f $appName ]; then
  echo "Make sure you are in the project directory"
  exit 1
 fi
 
 echo "Starting app server"
 
-forever dist/app
+forever $appName
